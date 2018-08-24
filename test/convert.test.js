4d353c8e5835998d7a4b89ed67189622f37d42b9
swjo <jjo6734@gmail.com>
2018-08-25 00:11:39 +0800

    Use Puppeteer to convert delta to html

--- test/convert.test.js
+++ test/convert.test.js
@@ -4,13 +4,25 @@ import { convertDeltaToHtml } from '../lib';
 import assert from 'assert';
 
 describe('convertDeltaToHtml', function() {
-  it('convert codeblock', function() {
-    const html = convertDeltaToHtml(mockOps.codeBlock);
-    console.log(html);
+  it('convert code block', function(done) {
+    convertDeltaToHtml(mockOps.codeBlock)
+      .then((html) => {
+        console.log(html);
+        if (html.indexOf('ql-code-block-container') > -1) {
+          assert.ok(1);
+          done();
+        }
+      });
   });
 
-  it('convert table', function() {
-    const html = convertDeltaToHtml(mockOps.table);
-    console.log(html);
+  it('convert table', function(done) {
+    convertDeltaToHtml(mockOps.table)
+      .then((html) => {
+        console.log(html);
+        if (html.indexOf('table-wrapper') > -1) {
+          assert.ok(1);
+          done();
+        }
+      });
   });
 });
