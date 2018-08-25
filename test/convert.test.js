a9b8eb861dd56e27b03a792cf78831aae2ae9deb
swjo <jjo6734@gmail.com>
2018-08-25 10:52:33 +0800

    Reorganize files

--- test/convert.test.js
+++ test/convert.test.js
@@ -3,24 +3,24 @@ import { mockOps } from './mock';
 import { convertDeltaToHtml } from '../lib';
 import assert from 'assert';
 
-describe('convertDeltaToHtml', function() {
-  it('convert code block', function(done) {
+describe('convertDeltaToHtml', () => {
+  it('convert code block', (done) => {
     convertDeltaToHtml(mockOps.codeBlock)
       .then((html) => {
         console.log(html);
         if (html.indexOf('ql-code-block-container') > -1) {
-          assert.ok(1);
+          assert.ok(true);
           done();
         }
       });
   });
 
-  it('convert table', function(done) {
+  it('convert table', (done) => {
     convertDeltaToHtml(mockOps.table)
       .then((html) => {
         console.log(html);
         if (html.indexOf('table-wrapper') > -1) {
-          assert.ok(1);
+          assert.ok(true);
           done();
         }
       });
