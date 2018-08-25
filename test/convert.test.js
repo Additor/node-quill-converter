b2b5c5d974cc49fdf1d9fa49fe7575c5f6e4bc70
swjo <jjo6734@gmail.com>
2018-08-25 10:57:26 +0800

    Convert Promise to async/await

--- test/convert.test.js
+++ test/convert.test.js
@@ -5,24 +5,24 @@ import assert from 'assert';
 
 describe('convertDeltaToHtml', () => {
   it('convert code block', (done) => {
-    convertDeltaToHtml(mockOps.codeBlock)
-      .then((html) => {
-        console.log(html);
-        if (html.indexOf('ql-code-block-container') > -1) {
-          assert.ok(true);
-          done();
-        }
-      });
+    (async () => {
+      const html = await convertDeltaToHtml(mockOps.codeBlock);
+      console.log(html);
+      if (html.indexOf('ql-code-block-container') > -1) {
+        assert.ok(true);
+        done();
+      }
+    })();
   });
 
   it('convert table', (done) => {
-    convertDeltaToHtml(mockOps.table)
-      .then((html) => {
-        console.log(html);
-        if (html.indexOf('table-wrapper') > -1) {
-          assert.ok(true);
-          done();
-        }
-      });
+    (async () => {
+      const html = await convertDeltaToHtml(mockOps.table);
+      console.log(html);
+      if (html.indexOf('table-wrapper') > -1) {
+        assert.ok(true);
+        done();
+      }
+    })();
   });
 });
